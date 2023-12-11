03f638a63643fae877b03be3bb6ec327dab681dd
MRchenyuheng <1351965541@qq.com>
2023-12-11 21:15:38 +0800

    20231211xx

---
+++ OS_work/OS_work/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace OS_work
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.Bisque;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(54, 78);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(270, 236);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(124, 354);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(185, 384);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(116, 72);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(374, 78);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowTemplate.Height = 23;
+            this.dataGridView2.Size = new System.Drawing.Size(261, 236);
+            this.dataGridView2.TabIndex = 3;
+            // 
+            // dataGridView3
+            // 
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.Location = new System.Drawing.Point(692, 78);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.RowTemplate.Height = 23;
+            this.dataGridView3.Size = new System.Drawing.Size(262, 236);
+            this.dataGridView3.TabIndex = 4;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(546, 384);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(126, 62);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1011, 580);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView3);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridView dataGridView3;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ OS_work/OS_work/Form1.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.Xml.Linq;
+using System.Threading;
+
+namespace OS_work
+{
+    public partial class Form1 : Form
+    {
+        struct mydata
+        {
+            public int id;
+            public int times;
+
+            public mydata(int x, int y)
+            {
+                id = x;
+                times= y;
+            }
+
+        }
+
+        bool[] idst = new bool[10020];
+
+        public DataTable tb1;
+        public DataTable tb2;
+        public DataTable tb3;
+
+        LinkedList<mydata> que1 = new LinkedList<mydata>();
+        LinkedList<mydata> que2 = new LinkedList<mydata>();
+        LinkedList<mydata> que3 = new LinkedList<mydata>();
+
+        bool f = false;
+        public Form1()
+        {
+            
+            InitializeComponent();
+            DataGridViewCellStyle style = new DataGridViewCellStyle();
+            style.Alignment = DataGridViewContentAlignment.MiddleCenter; // 调整对齐方式，可以根据实际需要修改
+            dataGridView1.DefaultCellStyle = style;
+
+            // 如果表头需要居中对齐，也可以添加以下代码
+            dataGridView1.ColumnHeadersDefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public void add_data(int id,int time,int op)
+        {
+            if (op == 1)
+            {
+
+                DataRow newRow = tb1.NewRow();
+                newRow["ID"] = id;
+                newRow["TIME_LEFT"] = time;
+                tb1.Rows.Add(newRow);
+            }
+
+            if (op == 2)
+            {
+
+                DataRow newRow = tb2.NewRow();
+                newRow["ID"] = id;
+                newRow["TIME_LEFT"] = time;
+                tb2.Rows.Add(newRow);
+            }
+
+            if (op == 3)
+            {
+
+                DataRow newRow = tb3.NewRow();
+                newRow["ID"] = id;
+                newRow["TIME_LEFT"] = time;
+                tb3.Rows.Add(newRow);
+            }
+
+
+        }
+
+        public int get_num(int l,int r)
+        {
+            Random random = new Random();
+            
+            int res = random.Next(l,r + 1);
+            return res;
+
+        }
+
+        
+
+        public void show()
+        {
+      
+
+            tb1 = new DataTable("level 1");
+            tb1.Columns.Add("ID",typeof(int));
+            tb1.Columns.Add("TIME_LEFT", typeof(int));
+
+            tb2 = new DataTable("level 2");
+            tb2.Columns.Add("ID", typeof(int));
+            tb2.Columns.Add("TIME_LEFT", typeof(int));
+
+            tb3 = new DataTable("level 3");
+            tb3.Columns.Add("ID", typeof(int));
+            tb3.Columns.Add("TIME_LEFT", typeof(int));
+
+
+            foreach (mydata t in que1)
+            {
+                add_data(t.id, t.times, 1);    
+            }
+
+            foreach (mydata t in que2)
+            {
+                add_data(t.id, t.times, 2);
+            }
+
+            foreach (mydata t in que3)
+            {
+                add_data(t.id, t.times, 3);
+            }
+
+
+
+            dataGridView1.DataSource = tb1;
+            dataGridView2.DataSource = tb2;
+            dataGridView3.DataSource = tb3;
+        }
+
+        public void solve()
+        {
+            f = false;
+            for (int i = 0; i < 10010; i++) idst[i] = false;
+
+            while (true)
+            {
+                Thread.Sleep(2000);
+                if (f == true) break;
+
+                int ok = get_num(1, 100);
+                ok %= 10;
+                if(ok == 0)
+                {
+                    int newid = get_num(1, 100000);
+                    newid %= 10007;
+                    if (idst[newid] == false)
+                    {
+                        int newtime = get_num(1, 100);
+                        newtime %= 20;
+
+                        mydata t = new mydata();
+                        t.id = newid;
+                        t.times = newtime;
+                        que1.AddLast(t);
+
+                    }
+                }
+
+                show();
+            }
+            
+            show();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            solve();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            f = true;
+        }
+    }
+}
