2460ba2c59fffa410cdec700967bfb1958d95db3
MRchenyuheng <1351965541@qq.com>
2023-12-11 23:03:44 +0800

    0231211okyesye

--- OS_work/OS_work/Form1.Designer.cs
+++ OS_work/OS_work/Form1.Designer.cs
@@ -34,6 +34,7 @@
             this.dataGridView2 = new System.Windows.Forms.DataGridView();
             this.dataGridView3 = new System.Windows.Forms.DataGridView();
             this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
@@ -41,12 +42,12 @@
             // 
             // dataGridView1
             // 
-            this.dataGridView1.BackgroundColor = System.Drawing.Color.Bisque;
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.MistyRose;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(54, 78);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowTemplate.Height = 23;
-            this.dataGridView1.Size = new System.Drawing.Size(270, 236);
+            this.dataGridView1.Size = new System.Drawing.Size(270, 264);
             this.dataGridView1.TabIndex = 0;
             this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
@@ -61,47 +62,67 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(185, 384);
+            this.button1.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.button1.Font = new System.Drawing.Font("宋体", 12F);
+            this.button1.Location = new System.Drawing.Point(407, 452);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(116, 72);
             this.button1.TabIndex = 2;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Text = "添加";
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // dataGridView2
             // 
+            this.dataGridView2.BackgroundColor = System.Drawing.Color.PeachPuff;
             this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView2.Location = new System.Drawing.Point(374, 78);
             this.dataGridView2.Name = "dataGridView2";
             this.dataGridView2.RowTemplate.Height = 23;
-            this.dataGridView2.Size = new System.Drawing.Size(261, 236);
+            this.dataGridView2.Size = new System.Drawing.Size(261, 264);
             this.dataGridView2.TabIndex = 3;
             // 
             // dataGridView3
             // 
+            this.dataGridView3.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
             this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView3.Location = new System.Drawing.Point(692, 78);
             this.dataGridView3.Name = "dataGridView3";
             this.dataGridView3.RowTemplate.Height = 23;
-            this.dataGridView3.Size = new System.Drawing.Size(262, 236);
+            this.dataGridView3.Size = new System.Drawing.Size(262, 264);
             this.dataGridView3.TabIndex = 4;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(546, 384);
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.button2.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button2.Location = new System.Drawing.Point(127, 522);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(126, 62);
+            this.button2.Size = new System.Drawing.Size(112, 72);
             this.button2.TabIndex = 5;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Text = "暂停";
+            this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.PaleGreen;
+            this.button3.Font = new System.Drawing.Font("宋体", 12F);
+            this.button3.Location = new System.Drawing.Point(126, 398);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(113, 72);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "开始";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1011, 580);
+            this.BackColor = System.Drawing.Color.LavenderBlush;
+            this.ClientSize = new System.Drawing.Size(1011, 694);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.dataGridView3);
             this.Controls.Add(this.dataGridView2);
@@ -110,6 +131,7 @@
             this.Controls.Add(this.dataGridView1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
@@ -126,6 +148,7 @@
         private System.Windows.Forms.DataGridView dataGridView2;
         private System.Windows.Forms.DataGridView dataGridView3;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- OS_work/OS_work/Form1.cs
+++ OS_work/OS_work/Form1.cs
@@ -13,12 +13,14 @@ using System.Threading;
 
 namespace OS_work
 {
+    
     public partial class Form1 : Form
     {
         struct mydata
         {
             public int id;
             public int times;
+
 
             public mydata(int x, int y)
             {
@@ -34,11 +36,15 @@ namespace OS_work
         public DataTable tb2;
         public DataTable tb3;
 
+        public int cnt1 = 0;
+        public int cnt2 = 0;
+        public int cnt3 = 0;
+
         LinkedList<mydata> que1 = new LinkedList<mydata>();
         LinkedList<mydata> que2 = new LinkedList<mydata>();
         LinkedList<mydata> que3 = new LinkedList<mydata>();
 
-        bool f = false;
+       private  bool f = true;
         public Form1()
         {
             
@@ -145,50 +151,140 @@ namespace OS_work
             dataGridView3.DataSource = tb3;
         }
 
-        public void solve()
+        public void add()
         {
-            f = false;
+          //f = false;
             for (int i = 0; i < 10010; i++) idst[i] = false;
 
-            while (true)
+            int newid = get_num(1, 100000);
+            newid %= 10007;
+            if (idst[newid] == false)
             {
-                Thread.Sleep(2000);
-                if (f == true) break;
+                int newtime = get_num(1, 100);
+                newtime %= 20;
 
-                int ok = get_num(1, 100);
-                ok %= 10;
-                if(ok == 0)
+                newtime += 1;
+
+                mydata t = new mydata();
+                t.id = newid;
+                t.times = newtime;
+                que1.AddLast(t);
+
+            }
+
+            show();
+
+        }
+
+        public void process()
+        {
+            mydata tmp;
+
+            if (que1.Count() > 0)
+            {
+                tmp = que1.First();
+                que1.RemoveFirst();
+                tmp.times--;
+                if(tmp.times > 0)
                 {
-                    int newid = get_num(1, 100000);
-                    newid %= 10007;
-                    if (idst[newid] == false)
-                    {
-                        int newtime = get_num(1, 100);
-                        newtime %= 20;
+                    que2.AddLast(tmp);
+                }
+                else idst[tmp.id] = false;
 
-                        mydata t = new mydata();
-                        t.id = newid;
-                        t.times = newtime;
-                        que1.AddLast(t);
+            }
+            else if(que2.Count > 0)
+            {
+                tmp = que2.First();
+                que2.RemoveFirst();
+                cnt2++;
+
+                tmp.times--;
+
+                if(tmp.times == 0)
+                {
+                    idst[tmp.id] = false;
+                    cnt2 = 0;
+
+                }
+                else
+                {
+                    if (cnt2 == 2)
+                    {
+                        cnt2 = 0;
+                        que3.AddLast(tmp);
 
                     }
+                    else que2.AddFirst(tmp);
                 }
 
-                show();
             }
-            
+            else if(que3.Count > 0)
+            {
+                tmp = que3.First();
+                que3.RemoveFirst();
+                cnt3++;
+
+                tmp.times--;
+
+                if (tmp.times == 0)
+                {
+                    idst[tmp.id] = false;
+                    cnt3 = 0;
+
+                }
+                else
+                {
+                    if (cnt3 == 4)
+                    {
+                        cnt3 = 0;
+                        que3.AddLast(tmp);
+
+                    }
+                    else que3.AddFirst(tmp);
+                }
+
+            }
+
             show();
 
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            solve();
+            f = false;
+            add();
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
+            f = false;
+        }
+
+   
+
+        private void button3_Click(object sender, EventArgs e)
+        {
             f = true;
+            //int cnt = 0;
+            while (f)
+            {
+                //cnt++;
+                //cnt %= 6;
+                //if (cnt == 0) add();
+              //  add();
+
+                process();
+                Thread.Sleep(1500);
+               Application.DoEvents();
+
+            }
+
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
